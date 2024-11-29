219f8c7e53b82015aca0a8b39c3ee1279974366c
Gopivaraprasad126 <chgopiyp@gmail.com>
2024-11-29 07:13:49 +0530

    commiting code and example file

---
+++ code.java
@@ -0,0 +1,10 @@
+
+public class Main{
+    public static void main(String[]args){
+        String gvp = "gvp";
+        String pvp = "Gopivaraprasad";
+        int age = 29;
+        String area = "Dilsukhnagar";
+
+    }
+}
